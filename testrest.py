70ccd12ce676088027e7dc7bc7ef27c5ce7989fc
Jame's Code <amanurxd@gmail.com>
2025-11-01 14:36:13 -0500

    Update testrest.py

--- testrest.py
+++ testrest.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import time
 import threading
+import os
 
 app = Flask(__name__)
 
@@ -91,4 +92,6 @@ def launch():
     return jsonify({"message": "Attack launched successfully!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable or use 10000 as default
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
